7a873f8053cbed245efb1cc7f0a610530b6754da
Pablo Farias <pablofdeafarias268@gmail.com>
2026-03-06 15:00:12 -0300

    Passo 1 preparacao dos dados

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+vocab = {
+    "o": 0,
+    "banco": 1,
+    "bloqueou": 2,
+    "cartao": 3
+}
+
+df_vocab = pd.DataFrame(list(vocab.items()), columns=["palavra", "id"])
+
+print("VOCABULÁRIO")
+print(df_vocab)
+
+sentence = ["o", "banco", "bloqueou", "cartao"]
+
+ids = [vocab[word] for word in sentence]
+
+print("\nIDS DA FRASE:")
+print(ids)
+
+d_model = 64
+
+vocab_size = len(vocab)
+
+embedding_table = np.random.randn(vocab_size, d_model)
+
+X = embedding_table[ids]
+
+X = np.expand_dims(X, axis=0)
+
+print("\nShape do tensor X:")
+print(X.shape)
